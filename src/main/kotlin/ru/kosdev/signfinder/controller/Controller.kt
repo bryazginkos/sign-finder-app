892e697ca2502db45e76a3f169615d1cef12fbcb
bryazginkos <bryazginkos@yahoo.com>
2015-09-22 22:49:21 +0600

    kotlin

---
+++ src/main/kotlin/ru/kosdev/signfinder/controller/Controller.kt
@@ -0,0 +1,38 @@
+package ru.kosdev.signfinder.controller
+
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import ru.kosdev.signfinder.SignDetector
+import ru.kosdev.signfinder.SignPlaceNotFoundException
+import ru.kosdev.signfinder.json.Result
+
+/**
+ * Created by Константин on 22.09.2015.
+ */
+@RestController
+public class Controller {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    @Autowired
+    private val signDetector: SignDetector? = null
+
+    @RequestMapping("/signfinder")
+    public fun hasSign(@RequestParam(value = "url") url: String): Result? {
+        log.info("try to find sign in file: " + url)
+        try {
+            val hasSign = signDetector!!.detectSign(url)
+            log.info("file $url. Result = $hasSign")
+            return Result(hasSign)
+        } catch (e: SignPlaceNotFoundException) {
+            log.info("Sign place is not found in $url")
+            return null
+        } catch (e: Exception) {
+            log.error(e.getMessage(), e)
+            return null
+        }
+    }
+}
