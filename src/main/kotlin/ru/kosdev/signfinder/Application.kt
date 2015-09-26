549215c367d13ec9b2d3376489f0e8551c441ac7
bryazginkos <bryazginkos@yahoo.com>
2015-09-26 23:51:11 +0600

    kotlin Application

---
+++ src/main/kotlin/ru/kosdev/signfinder/Application.kt
@@ -0,0 +1,42 @@
+package ru.kosdev.signfinder
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.*
+import ru.kosdev.signfinder.transform.RotateTransformer
+
+/**
+ * Created by Константин on 26.09.2015.
+ */
+@SpringBootApplication
+@PropertySources(value = *arrayOf(PropertySource("classpath:signfinder.properties")))
+@ComponentScan
+@Configuration
+public open class Application {
+
+    @Value("\${angle.correction}")
+    private val correctionAngle: Int = 0
+
+    @Bean(name = arrayOf("rotatePlusTransformer"))
+    public open fun rotatePlusTransformer(): RotateTransformer {
+        return RotateTransformer(correctionAngle.toDouble())
+    }
+
+    @Bean(name = arrayOf("rotateMinusTransformer"))
+    public open fun rotateMinusTransformer(): RotateTransformer {
+        return RotateTransformer((-correctionAngle).toDouble())
+    }
+
+    @Bean(name = arrayOf("upsideDownTransformer"))
+    public open fun upsideDownRotateTransformer(): RotateTransformer {
+        return RotateTransformer(180.0)
+    }
+
+    companion object {
+        @JvmStatic
+        public fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
