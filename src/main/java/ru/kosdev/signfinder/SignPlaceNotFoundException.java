b6f95a9e613e11bdb70a32de5c2530e326a451cc
bryazginkos <bryazginkos@yahoo.com>
2015-09-27 00:37:47 +0600

    kotlin SignNotFoundException

--- src/main/java/ru/kosdev/signfinder/SignPlaceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.kosdev.signfinder;
-
-/**
- * Created by brjazgin on 27.08.2015.
- */
-public class SignPlaceNotFoundException extends Exception {
-}
---
+++ src/main/kotlin/ru/kosdev/signfinder/SignPlaceNotFoundException.kt
@@ -0,0 +1,6 @@
+package ru.kosdev.signfinder
+
+/**
+ * Created by Константин on 27.09.2015.
+ */
+public class SignPlaceNotFoundException (message: String? = null, cause: Throwable? = null) : Throwable (message, cause)
